ce60ea467eb2d70c86c099254500a091edf1b897
Aravinth-IVY <aravinth.r@ivymobility.com>
2021-10-21 07:47:29 +0530

    Initial commit

---
+++ arthimetic/src/main/java/com/example/arthimetic/Arthimetic.java
@@ -0,0 +1,20 @@
+package com.example.arthimetic;
+
+public class Arthimetic {
+    public static float Add(float a,float b){
+        float c=a+b;
+        return c;
+    }
+    public static float sub(float a,float b){
+        float c=a-b;
+        return c;
+    }
+    public static float mul(float a,float b){
+        float c=a*b;
+        return c;
+    }
+    public static float div(float a,float b){
+        float c=a/b;
+        return c;
+    }
+}
